b1db8f3128097b377f5a575e510eec01b61aeadd
Jack <958691165@qq.com>
2018-09-21 09:19:02 +0800

    a

--- src/api.js
+++ src/api.js
@@ -6,11 +6,12 @@ var config=require('../config');
 router.get('/getConfig',function (req,res) {
     var file=req.query.file;
     var content=fs.readFileSync(config.vhost_dir+'/'+file).toString();
-    var sname=content.match(/server_name(.*?);/)[1];
-    var sroot=content.match(/root(.*?);/)[1];
+    // var sname=content.match(/server_name(.*?);/)[1];
+    // var sroot=content.match(/root(.*?);/)[1];
     res.send({
-        vhost:sname.trim(),
-        root:sroot.trim()
+        // vhost:sname.trim(),
+        // root:sroot.trim()
+        content:content
     });
 });
 
@@ -42,11 +43,12 @@ router.post('/editVhost',function (req,res) {
     var file=req.body.name;
     require('child_process').exec('rm -f '+config.vhost_dir+'/'+file);
 
-    var sname=req.body.vhost;
-    var sroot=req.body.dir;
+    // var sname=req.body.vhost;
+    // var sroot=req.body.dir;
+    var content=req.body.content;
 
-    var content=config.vhost_template.replace("[#server_name#]",sname)
-        .replace('[#root#]',sroot);
+    // var content=config.vhost_template.replace("[#server_name#]",sname)
+    //     .replace('[#root#]',sroot);
 
     fs.writeFileSync(config.vhost_dir+'/'+sname+'.conf',content);
     require('child_process').exec('service nginx restart');
